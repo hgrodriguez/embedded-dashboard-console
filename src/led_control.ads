80f66891fb529f11dcb4e212934d4fa300eecd6f
hrodriguez <holger.rodriguez@roseng.ch>
2021-09-29 17:32:48 +0200

    LED_Control package documented

--- src/led_control.ads
+++ src/led_control.ads
@@ -1,29 +1,53 @@
+--===========================================================================
+--
+--  This package offers the control for the LEDs
+--
+--===========================================================================
 --  Copyright 2021 (C) Holger Rodriguez
 --
 --  SPDX-License-Identifier: BSD-3-Clause
 --
 package LED_Control is
 
+   --------------------------------------------------------------------------
+   --  All Red LED related controls
+   --------------------------------------------------------------------------
    procedure LED_0_Red_Off;
    procedure LED_0_Red_On;
    procedure LED_0_Red_Toggle;
 
+   --------------------------------------------------------------------------
+   --  All Amber LED related controls
+   --------------------------------------------------------------------------
    procedure LED_0_Amber_Off;
    procedure LED_0_Amber_On;
    procedure LED_0_Amber_Toggle;
 
+   --------------------------------------------------------------------------
+   --  All Green LED related controls
+   --------------------------------------------------------------------------
    procedure LED_0_Green_Off;
    procedure LED_0_Green_On;
    procedure LED_0_Green_Toggle;
 
+   --------------------------------------------------------------------------
+   --  All White LED related controls
+   --------------------------------------------------------------------------
    procedure LED_0_White_Off;
    procedure LED_0_White_On;
    procedure LED_0_White_Toggle;
 
+   --------------------------------------------------------------------------
+   --  All Blue LED related controls
+   --------------------------------------------------------------------------
    procedure LED_0_Blue_Off;
    procedure LED_0_Blue_On;
    procedure LED_0_Blue_Toggle;
 
+   --------------------------------------------------------------------------
+   --  This is just here for the sole reason, that all LEDs can be toggled
+   --  for a quick visual check
+   --------------------------------------------------------------------------
    procedure Toggle_All_LEDs (Delay_Between_In_Milliseconds : Integer);
 
 end LED_Control;
