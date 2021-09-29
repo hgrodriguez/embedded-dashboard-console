aaa0611ec144371b51f397860fd60f57764e2631
hrodriguez <holger.rodriguez@roseng.ch>
2021-09-29 14:16:16 +0200

    LED: added Handler.Serial package, moved code from Edc to this new package, added copyright/license notes

--- src/edc.adb
+++ src/edc.adb
@@ -8,34 +8,14 @@ with RP.Clock;
 with RP.GPIO;
 
 with ItsyBitsy;
-with LED_Control;
 
+with Handler.Serial;
 with Transport.Serial;
-with Evaluate;
-with Evaluate.LEDs;
-
-with Execute;
-with Execute.LEDs;
 
 procedure Edc is
    WAIT_FOR_NEXT_LED : constant Integer := 25;
 
    Area_Selector     : Transport.Area_Selector;
-
-   procedure Process_LED;
-   procedure Process_LED is
-      LED_Instruction : Evaluate.LEDs.LED_Instruction;
-      LED_Error       : Execute.LED_Errors;
-      LED_Action      : Execute.LED_Actions;
-      use Execute;
-   begin
-      LED_Instruction := Transport.Serial.Get_LED_Instruction;
-      LED_Error := Evaluate.LEDs.Check_Input (LED_Instruction);
-      if LED_Error = Execute.OK then
-         LED_Action := Evaluate.LEDs.Evaluate (LED_Instruction);
-         Execute.LEDs.Execute (LED_Action);
-      end if;
-   end Process_LED;
 
    use Transport;
 
@@ -51,14 +31,8 @@ begin
       --  Check for Serial Channel input
       Area_Selector := Transport.Serial.Get_Area_Selector;
       if Area_Selector /= Transport.None then
-
-         --  something arrived on the UART, process it
-         case Area_Selector is
-            when Evaluate.LED_Prefix =>
-               Process_LED;
-            when others =>
-               LED_Control.Toggle_All_LEDs (WAIT_FOR_NEXT_LED);
-         end case;
+         --  something arrived on serial, handle it
+         Handler.Serial.Handle_Request (Area_Selector);
       end if;
    end loop;
 end Edc;
